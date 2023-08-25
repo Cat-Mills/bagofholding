7e7ce7053d5a397c4a0f28c622cb26a3e6323e08
Cat-Mills <141058467+Cat-Mills@users.noreply.github.com>
2023-08-25 11:03:12 -0600

    virgin

---
+++ server/app.js
@@ -0,0 +1,33 @@
+//imports
+import express from 'express';
+import morgan from 'morgan';
+import ViteExpress from 'vite-express';
+import handlerFunctions from './controller';
+
+//app instance
+const app = express();
+const port = '2222';
+
+//middleware
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+
+ViteExpress.config({ printViteDevServerHost: true });
+
+// Routes go here
+const {addItem, deleteItem, editItem, getInventory} = handlerFunctions
+
+app.get('/inventory', getInventory)
+app.post('/addItem', addItem)
+app.delete('/removeItem/:id', deleteItem)
+app.put('/editInventory/:id', editItem)
+
+
+
+
+
+
+
+
+ViteExpress.listen(app, port, () => console.log(`Investigation Check successful. You found http://localhost:${port}`));
---
+++ server/controller.js
@@ -0,0 +1,55 @@
+
+let TEST_ITEMS = [
+    {id: 0, name: 'Hand Axe', qty: 2, value: 50, type: 'Weapon'},
+    {id: 1, name: 'Torch', qty: 1, value: 10, type: 'Tool'},
+    {id: 2, name: 'Jerky', qty: 5, value: 15, type: 'Food'},
+]
+
+let globalId = 3
+
+const handlerFunctions = {
+
+    //? Sends the Test items to the front end when getInventory is invoked.
+    getInventory: (req,res) => {
+        res.send(TEST_ITEMS)
+    },
+
+    addItem: (req,res) => {
+        const {name} = req.body
+
+        const newItem = {
+            id: globalId,
+            name,
+            qty,
+            value,
+            type
+        }
+        TEST_ITEMS.push(newItem)
+        globalId++
+        res.send(newItem)
+    },
+
+    deleteItem: (req,res) => {
+        const {id} = req.params
+
+        let filteredList = TEST_ITEMS.filter(el => el.id !== +id)
+        TEST_ITEMS = filteredList
+        res.send('Item removed')
+    },
+
+    editItem: (req,res) => {
+        const {id} = req.params
+        const {name,qty,value,type} = req.body
+        let index = TEST_ITEMS.findIndex(el => el.id === +id)
+        let item = TEST_ITEMS[index]
+
+        item.name = name ?? item.name
+        item.qty = +qty ?? item.qty
+        item.value = +value ?? item.value
+        item.type = type ?? item.type
+
+        res.send(item)
+    }
+}
+
+export default handlerFunctions
